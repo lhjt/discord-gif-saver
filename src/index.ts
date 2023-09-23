c1e106121e67b216f2481e4ec2fe2503ed9b4d59
Jared L <48422312+lhjt@users.noreply.github.com>
2023-09-23 12:48:15 +1000

    chore(style): run formatting

--- src/index.ts
+++ src/index.ts
@@ -4,66 +4,66 @@ import cliProgress from "cli-progress";
 import filetype from "magic-bytes.js";
 
 interface GifRecord {
-  name: string;
-  url: string;
-  width: number;
-  height: number;
+    name: string;
+    url: string;
+    width: number;
+    height: number;
 }
 
-const b1 = new cliProgress.SingleBar({
-  format:
-    "CLI Progress |" +
-    colors.cyan("{bar}") +
-    "| {percentage}% || {value}/{total} Chunks || Speed: {speed}",
-  barCompleteChar: "\u2588",
-  barIncompleteChar: "\u2591",
-  hideCursor: true,
+const pb = new cliProgress.SingleBar({
+    format:
+        "CLI Progress |" +
+        colors.cyan("{bar}") +
+        "| {percentage}% || {value}/{total} Chunks",
+    barCompleteChar: "\u2588",
+    barIncompleteChar: "\u2591",
+    hideCursor: true,
 });
 
 const file = await Bun.file("src/binary.bin.json").text();
 const records: GifRecord[] = (JSON.parse(file) as any[]).map((r) => ({
-  name: r["1"].split("/").slice(-1)[0],
-  url: r["2"]["2"],
-  width: r["2"]["3"],
-  height: r["2"]["4"],
+    name: r["1"].split("/").slice(-1)[0],
+    url: r["2"]["2"],
+    width: r["2"]["3"],
+    height: r["2"]["4"],
 }));
 
 const noLongerFound: string[] = [];
 
 async function saveFile(record: GifRecord) {
-  const buffer = await fetch(record.url).then((r) => r.arrayBuffer());
-  const fileType = filetype(new Uint8Array(buffer));
-  console.log(fileType);
-  console.log(record.name);
+    const buffer = await fetch(record.url).then((r) => r.arrayBuffer());
+    const fileType = filetype(new Uint8Array(buffer));
+    console.log(fileType);
+    console.log(record.name);
 
-  if (fileType.length === 0) {
-    console.error(`Image ${record.name} no longer exists :(`);
-    noLongerFound.push(record.name);
-    return;
-  }
+    if (fileType.length === 0) {
+        console.error(`Image ${record.name} no longer exists :(`);
+        noLongerFound.push(record.name);
+        return;
+    }
 
-  // Write file
-  const hasher = new Bun.CryptoHasher("sha256");
-  hasher.update(buffer);
-  const hash = hasher.digest("hex").slice(0, 8);
+    // Write file
+    const hasher = new Bun.CryptoHasher("sha256");
+    hasher.update(buffer);
+    const hash = hasher.digest("hex").slice(0, 8);
 
-  const extension = fileType[0].typename;
-  const name = record.name.replace(`.${extension}`, "");
-  const finalName = `${name}-${hash}.${extension}`;
+    const extension = fileType[0].typename;
+    const name = record.name.replace(`.${extension}`, "");
+    const finalName = `${name}-${hash}.${extension}`;
 
-  await Bun.write(`output/${finalName}`, buffer);
+    await Bun.write(`output/${finalName}`, buffer);
 }
 
 const myq = new Queue(5, 100);
 
-b1.start(records.length, 0);
+pb.start(records.length, 0);
 for (const record of records) {
-  const me = Symbol();
-  myq.wait(me);
+    const me = Symbol();
+    myq.wait(me);
 
-  await saveFile(record);
-  b1.increment();
+    await saveFile(record);
+    pb.increment();
 }
 
-b1.stop();
+pb.stop();
 console.log("No longer found", noLongerFound);
